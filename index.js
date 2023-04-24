cba771ae5110d3ac55bfae3a0e1c00eb838488a7
Chloe Garza <cmgarza52@gmail.com>
2023-04-24 04:45:55 -0700

    complete

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance < 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  let response = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return response;
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default: "thanks for everything"
+    return 'Bye.';
+  }
 }
